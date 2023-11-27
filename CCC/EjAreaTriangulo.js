d9a960f24d0e9caca6431de965a496eeae4fa6a0
Paula Cano <paulacanoroman@gmail.com>
2023-11-27 20:46:18 +0100

    Ejercicio area triangulo

---
+++ CCC/EjAreaTriangulo.js
@@ -0,0 +1,10 @@
+
+function area(){
+    let base, altura, resultado;
+
+    base=parseInt(prompt("introduce una base"));
+    altura=parseInt(prompt("introduce una altura"));
+    resultado=(base*altura)/2;
+    
+    return alert("el area es " + resultado);
+}
