9903ed01d2b94bf8e86ba1f30cbb2700247c0166
Paula Cano <paulacanoroman@gmail.com>
2023-12-13 17:27:18 +0100

    sumar arrays

---
+++ Arrays/EjercicioSumaArr.js
@@ -0,0 +1,12 @@
+function pinta1(numeros) 
+    {
+        var i=0,suma=0;
+        document.write("<table border=1><tr>");
+        while (i<numeros.length)
+        {	
+            document.write("<td>"+numeros[i]+"</td>");	
+        suma=suma+numeros[i]; //lo sustituye por .... y pone varias respuestas
+            i=i+1;
+        }
+    dcoument.write(" la suma de todos los numeros introducidos en la tabla es "+suma);
+    }
